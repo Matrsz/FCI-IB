6c797ec9f07600fc765200f74e89b5a21c52f046
Matías Roqueta <matias.roqueta.sz@gmail.com>
2022-10-16 19:33:18 -0300

    rician fading

--- TP2/Lib.jl
+++ TP2/Lib.jl
@@ -2,6 +2,11 @@ module Lib
 
 export genClust, genScatt, genDists, genhl, delaySpread
 
+using FFTW
+using Trapz
+using Distributions
+using Statistics
+
 """ 
 **clust = genClust(N; minR, minDist)** 
 
@@ -16,11 +21,13 @@ function genClust(N::Int, minR::Real=0, minDist::Real=0)
     clust = []
     for i in 1:N
         xy = (0, 0)
+        j = 0
         while true
+            j = j+1
             r = sqrt(rand())
             t = 2π*rand()
             xy = (r*cos(t), r*sin(t))
-            hypot(xy...)<minR || any(p -> hypot((xy.-p)...) < minDist, clust) || break
+            hypot(xy...)<minR || any(p -> hypot((xy.-p)...) < minDist*0.9^j, clust) || break
         end
         push!(clust, xy)
     end
@@ -66,7 +73,21 @@ function genDists(S::Vector{Tuple{Float64,Float64}}, posTx::Tuple{Real,Real}, po
 end
 
 
+""" 
+**hl, tt = genhl(Dsts, fc, W, a_i, oversample, margin)**
 
+Esta funcion produce la respuesta impulsiva discreta equivalente banda base 
+del canal correspondiente a todos los caminos con distancias dadas por el 
+vector Dsts con atenuaciones a_i, a una frecuencia central fc y un ancho de banda W.
+Recibe los siguientes parámetros opcionales:
+
+oversample: Un parametro que debe ser un número entero mayor o igual a 1 y 
+que sirve para generar puntos intermedios entre las muestras (para simular 
+el comportamiento "analogico" del canal)
+
+margin: la respuesta se calcula para floor(margin)/2 muestras anteriores al
+primer eco (o camino) recibido del canal e idem para el ultimo recibido. 
+"""
 function genhl(dists, fc, W, a_i, oversample=1, margin=0)
     delays = dists./3e8
     groupdelay = minimum(delays)
@@ -82,14 +103,50 @@ function genhl(dists, fc, W, a_i, oversample=1, margin=0)
 end
 
 """ 
-Ds = delaySpread(Dsts)
+**Ds = delaySpread(Dsts)**
 
 Recibe como parametro un vector dists de distancias (en metros)
 y retorna el delay spread Td (en segundos) entre esas distancias.
 """ 
-function delaySpread(dists::Vector{Real})
+function delaySpread(dists::Vector{Float64})
     return (maximum(dists)-minimum(dists))/3e8
 end
 
+function genHl(hl, tt)
+    power(x, y) = trapz(x, abs.(y).^2)
+    ff = fftfreq(length(tt), 1/(tt[2]-tt[1])) |> fftshift
+    Hl = fft(hl) |> fftshift
+    Hl = Hl.*sqrt(power(tt, hl)/power(ff, Hl))
+    return Hl, ff
+end
+
+function simulateFlatFade(nRealz; Ns, Rs, Cs, posTx, posRx, fc, W, a_i, margin)
+    hs = []
+    for i in 1:nRealz
+        S, _ = Lib.genScatt(Ns,Rs,Cs)
+        dsts = Lib.genDists(S,posTx,posRx)
+        h, _ = Lib.genhl(dsts,fc,W,a_i,1,margin)
+        imax = argmax(abs.(h))
+        push!(hs, h[imax])
+    end
+    return hs
+end
+
+function toRician(a_i, k)
+    oldpower  = sum(abs.(a_i).^2)
+    a_i[1] = sqrt(k*sum(abs.(a_i[2:end]).^2))
+    newpower = sum(abs.(a_i).^2)
+    return a_i.*sqrt(oldpower/newpower)
+end
+
+function simulateFlatFade(nRealz, k; Ns, Rs, Cs, posTx, posRx, fc, W, a_i, margin)
+    a_i = toRician(a_i, k)
+    hs = simulateFlatFade(nRealz; Ns, Rs, Cs, posTx, posRx, fc, W, a_i, margin)
+    abs_hs, arg_hs = abs.(hs), angle.(hs)
+    omega = mean(abs_hs.^2)
+    sigma = sqrt(omega/(2*(k+1)))
+    nu = sigma*sqrt(2*k)
+    return hs, Rician(nu, sigma)  
+end
 end
 

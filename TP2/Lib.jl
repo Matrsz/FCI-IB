4231e68c9634ed27455774d586cb082d73218abe
Matías Roqueta <matias.roqueta.sz@gmail.com>
2022-11-02 13:50:42 -0300

    TP2 Finished

--- TP2/Lib.jl
+++ TP2/Lib.jl
@@ -17,7 +17,7 @@ El parámetro opcional minR indica una distancia mínima al centro del cluster q
 
 El parámetro opcional minDist indica una distancia mínima a otros elementos que deben tener los elementos.
 """
-function genClust(N::Int, minR::Real=0, minDist::Real=0)
+function genClust(N, minR=0, minDist=0)
     clust = []
     for i in 1:N
         xy = (0, 0)
@@ -50,7 +50,7 @@ Retorna S un vector de tuplas (x, y) representando las posiciones
 de los reflectores, y un vector de vectores Ss representando las
 posiciones de los reflectores agrupados en sus respectivos clusters.
 """
-function genScatt(Ns::Vector{Int}, Rs::Vector{Float64}, Cs::Vector{Tuple{Float64, Float64}})
+function genScatt(Ns, Rs, Cs)
     clust(N, R, C) = [C .+ R .* x for x in genClust(N)]
     cs = [clust(N, R, C) for (R, C, N) in zip(Rs, Cs, Ns)] 
     return [(cs... )... ], cs
@@ -66,10 +66,18 @@ de un emisor y receptor.
 Retorna un vector con las distancias de todos los posibles caminos entre 
 el emisor y el receptor.
 """
-function genDists(S::Vector{Tuple{Float64,Float64}}, posTx::Tuple{Real,Real}, posRx::Tuple{Real,Real})
+function genDists(S, posTx, posRx, return_TxRx=false)
     Tx_dists = [hypot((posTx.-s)...) for s in S]
     Rx_dists = [hypot((posRx.-s)...) for s in S]
+    if return_TxRx
+        return Tx_dists, Rx_dists
+    end
     return Tx_dists.+Rx_dists
+end
+
+
+function genAtts(dists)
+    return [-1/x for x in dists]
 end
 
 
@@ -108,7 +116,7 @@ end
 Recibe como parametro un vector dists de distancias (en metros)
 y retorna el delay spread Td (en segundos) entre esas distancias.
 """ 
-function delaySpread(dists::Vector{Float64})
+function delaySpread(dists)
     return (maximum(dists)-minimum(dists))/3e8
 end
 
@@ -129,7 +137,7 @@ function simulateFlatFade(nRealz; Ns, Rs, Cs, posTx, posRx, fc, W, a_i, margin)
         imax = argmax(abs.(h))
         push!(hs, h[imax])
     end
-    return hs
+    return hs./mean(abs.(hs))
 end
 
 function toRician(a_i, k)
@@ -142,11 +150,42 @@ end
 function simulateFlatFade(nRealz, k; Ns, Rs, Cs, posTx, posRx, fc, W, a_i, margin)
     a_i = toRician(a_i, k)
     hs = simulateFlatFade(nRealz; Ns, Rs, Cs, posTx, posRx, fc, W, a_i, margin)
-    abs_hs, arg_hs = abs.(hs), angle.(hs)
+    abs_hs = abs.(hs)
     omega = mean(abs_hs.^2)
     sigma = sqrt(omega/(2*(k+1)))
     nu = sigma*sqrt(2*k)
     return hs, Rician(nu, sigma)  
+end
+
+function simulateDoppler(posTx, x, S, fc, W, margin)
+    function geth0(xi)
+        Txd, Rxd = Lib.genDists(S,posTx,xi, true)
+        a_i = 1 ./(Txd.*Rxd)
+        h, _ = Lib.genhl(Txd.+Rxd,fc,W,a_i,1,margin)
+        imax = argmax(abs.(h))
+        return h[imax]
+    end
+    return geth0.(x)
+end
+
+function simulateClarke(t, x, nRealz; N, R, posTx, posRx, fc, W, margin)
+    function autocor(x)
+        M = x*x'
+        imid = Int(length(x)/2)
+        return [M[i, imid] for i in eachindex(x)]
+    end
+    S, _ = Lib.genScatt([N], [R], [posRx])
+    h = Lib.simulateDoppler(posTx, x.(t), S, fc, W, margin)
+    rs = zeros(Complex{Float64}, length(h), nRealz)
+    for i in 1:nRealz
+      S, _ = Lib.genScatt([N], [R], [posRx])
+      h = Lib.simulateDoppler(posTx, x.(t), S, fc, W, margin)
+      rs[:,i] = autocor(h)
+    end
+    r = [mean(rs[i,:]) for i in eachindex(t)] 
+    r = r ./ maximum(abs.(r))
+    tt = t.-t[end]/2
+    return tt, r
 end
 end
 
